3e1913aa17f37b74ac44669cd30c04cc58bc0c0c
i.aryka <i.aryka@hqsoftwarelab.com>
2020-01-10 17:21:17 +0300

    add glide support

--- testlibrary/src/main/java/com/ar/testlibrary/TestHelper.kt
+++ testlibrary/src/main/java/com/ar/testlibrary/TestHelper.kt
@@ -1,12 +1,20 @@
 package com.ar.testlibrary
 
 import android.content.Context
+import android.widget.ImageView
 import android.widget.Toast
+import com.bumptech.glide.Glide
 
 object TestHelper {
 
     public fun helpToast(context: Context) {
         Toast.makeText(context, "Help", Toast.LENGTH_LONG).show()
+    }
+
+    public fun helpImage(context: Context, url: String, imageView: ImageView){
+        Glide.with(context)
+            .load(url)
+            .into(imageView)
     }
 
 }
